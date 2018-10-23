aa35745e392c13adb6b2174e96e0dade160ace59
Alexandra Johnson <alexandra@queryclick.com>
2018-10-23 15:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+// Write your code in this f
+function scuberGreetingForFeet(distance) {
+  if (distance<=400) {
+    return 'This one is on me!'
+  }
+  if (distance>=2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance>2500) {
+    return 'No can do.'
+      }
+  
+}
+
+function ternaryCheckCity(city) {
+  return (isMember ? "$2.00" : "$10.00");
